8a80b2df227b2e2295827b930fa72a5f14df9423
Dmytro Manzhula <d.manzhula@gmail.com>
2018-07-12 23:38:13 +0300

    works without calendar

--- src/test/java/Loadtest.java
+++ src/test/java/Loadtest.java
@@ -1,6 +1,4 @@
-import jdk.internal.util.xml.impl.Input;
-import org.openqa.selenium.By;
-import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,12 +6,13 @@ import org.testng.annotations.Test;
 
 public class Loadtest {
 
-    private final String GECKO_DRIVER_PATH = "C:\\geckodriver\\geckodriver.exe";
+    private final String WINDOWS_GECKO_DRIVER_PATH = "C:\\geckodriver\\geckodriver.exe";
+
     private final String WEB_URL = "https://www.booking.com/index.en-gb.html";
 
     @Test
-    public void bookingLoadTest(){
-        System.setProperty("webdriver.gecko.driver", GECKO_DRIVER_PATH);
+    public void bookingLoadTest() throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", WINDOWS_GECKO_DRIVER_PATH);
         WebDriver webDriver = new FirefoxDriver();
 
         webDriver.get(WEB_URL);
@@ -21,20 +20,22 @@ public class Loadtest {
         WebElement city = ((FirefoxDriver) webDriver).findElementById("ss");
         city.sendKeys("New York");
 
-        WebElement dateBox = ((FirefoxDriver) webDriver)
-                .findElementByXPath("//html/body/div[3]/div/div/div[2]/form/div[1]/div[2]" +
-                        "/div/div[3]/div/div/div/div[1]/div/button");
-        dateBox.sendKeys("09/01/2018");
+        Thread.sleep(3000);
 
-        WebElement endDate;
+        WebElement dateBox = ((FirefoxDriver) webDriver).findElementByXPath("/html/body/div[3]/div/div/div[2]/" +
+                "form/div[1]/div[2]/div/div[2]/div/div/div/div[1]/div/div[1]/div[1]/select");
+
+        JavascriptExecutor js = (JavascriptExecutor)webDriver;
+
+        String date = "arguments[0].setAttribute('value','9-2018');";
+
+        js.executeScript(date, dateBox);
 
         WebElement searchButton = ((FirefoxDriver) webDriver)
-                .findElementByXPath("/html/body/div[3]/div/div/div[2]/form/div[1]/div[2]" +
-                        "/div/div[2]/div/div/div/div[1]/div/button/svg/path");
+                .findElementByXPath("/html/body/div[3]/div/div/div[2]/form/div[1]/div[4]/div[2]/button");
         searchButton.click();
 
-
-        //webDriver.quit();
+        webDriver.quit();
     }
 
 
